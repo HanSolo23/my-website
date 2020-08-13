9a40ee59c0cea5591fefa15d991c7d3fd75ec49d
HanSolo23 <vanotaran@gmail.com>
2020-08-13 18:23:50 +0300

    New design of portfolio

--- static/js/script.js
+++ static/js/script.js
@@ -3,31 +3,6 @@ $(document).ready(function() {
 		$('.header__burger,.header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
-	/*$('.slider').slick({
-		arrows:true,
-		dots:false,
-		adaptiveHeight:true,
-		slidesToShow:1,
-		slidesToScroll:1,
-		speed:500,
-		easing:'ease',
-		draggable:false,
-		touchThreshold: 10,
-		responsive: [
-			{
-				breakpoint: 1025,
-				settings: {
-					slidesToShow:2,
-				}
-
-			},{
-				breakpoint: 601,
-				settings: {
-					slidesToShow:1,
-				}
-			}
-		]
-	});*/
 });
 
 // Main settings
@@ -49,7 +24,7 @@ if (window.location.href.indexOf('portfolio') > -1) {
 	footerLinks.forEach((footerLink) => {
 		footerLink.style.display = 'none';
 	});
-	body.style.background = '#191919 center / cover no-repeat';
+	body.style.background = 'black center / cover no-repeat';
 };
 
 // Portfolio slider
@@ -59,16 +34,26 @@ var sliderItems = mainSlider.children;
 var arrows = document.querySelector('.arrows');
 var previous = arrows.querySelector('.prev');
 var next = arrows.querySelector('.next');
-var images = mainSlider.querySelectorAll('img')
+var images = mainSlider.querySelectorAll('img');
+var container = document.querySelector('.portfolio');
 
-/*var allImages = function(item) {
+var slidesToScroll = 3;
+var itemWidth = Math.round(container.clientWidth);
+var movePosition = itemWidth / slidesToScroll;
+
+var allImages = function(item) {
 	var elements = [];
 	for (var i = 0; i < item.length; i++) {
-		var element = item[i].children;
+		var element = item[i].querySelector('img');
 		elements.push(element);
 	}
 	return elements;
-};*/
+};
+
+var items = allImages(sliderItems);
+items.forEach((item) => {
+	item.style.maxWidth = `${itemWidth}px`;
+});
 
 var settings = {
 	width: 300,
@@ -76,37 +61,11 @@ var settings = {
 	margin: 20
 };
 
-
-var leftPosition = function(item, index){
-	var leftPoint = (settings.width + settings.margin) * index + 'px';
-	return item.style.left = leftPoint;
-};
-
-
-/*var topPosition = function(item, index) {
-	var topPoint = (settings.height + settings.margin) * index + 'px';
-	return item.style.top = topPoint; 
-};*/
-
-
-for (var i = 0; i < sliderItems.length; i++) {
-	leftPosition(sliderItems[i], i);
-};
-
-
-/*for (var j = 0; j < allImages(sliderItems).length; j++) {
-	for (var b = 0; b < allImages(sliderItems)[j].length; b++) {
-		topPosition(allImages(sliderItems)[j][b], b);
-	};	
-};
-*/
-
 var row = 0;
 next.addEventListener('click', function(evt) {
 	evt.preventDefault;
-	row -= settings.width + settings.margin;
-	var goTo = row + 'px';
-	mainSlider.style.left = goTo;
+	row -= movePosition;
+	mainSlider.style.transform = `translateX(${row}px)`;
 	if (-row === (sliderItems.length - 2) * (settings.width + settings.margin)) {
 		next.classList.add('disable');
 	};
@@ -118,9 +77,8 @@ next.addEventListener('click', function(evt) {
 
 previous.addEventListener('click', function(evt) {
 	evt.preventDefault;
-	row += settings.width + settings.margin;
-	var goTo = row + 'px';
-	mainSlider.style.left = goTo;
+	row += movePosition;
+	mainSlider.style.transform = `translateX(${row}px)`;
 	if (row === settings.width + settings.margin) {
 		previous.classList.add('disable');
 	};
